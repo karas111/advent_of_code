58a020e28e10b28234d8dc3585b15824bb60e6ce
Piotr Karasinski <piotr.karasinski@beacon.io>
2019-12-03 09:52:15 +0100

    Day 3 solution

---
+++ year2019/day3/a.py
@@ -0,0 +1,43 @@
+import os
+
+TRANSFORM_DIRECTION = {
+    'L': (-1, 0),
+    'R': (1, 0),
+    'U': (0, 1),
+    'D': (0, -1)
+}
+
+
+def transform_line(line_str):
+    for data in line_str.split(','):
+        yield (TRANSFORM_DIRECTION[data[0]], int(data[1:]))
+
+
+def read_wires():
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
+        return [list(transform_line(line)) for line in f]
+
+
+def get_wire_points(wire_direction, starting_point=(0, 0)):
+    for modifier, length in wire_direction:
+        for i in range(length):
+            starting_point = (starting_point[0] + modifier[0], starting_point[1] + modifier[1])
+            yield starting_point
+
+
+def calculate_distance(points):
+    return [sum(abs(x) for x in point) for point in points]
+
+
+def main():
+    wires = read_wires()
+    wires_points = [set(get_wire_points(wire)) for wire in wires]
+    common_points = wires_points[0]
+    for wire_points in wires_points[1:]:
+        common_points = common_points & wire_points
+    distances = calculate_distance(common_points)
+    print(min(distances))
+
+
+if __name__ == "__main__":
+    main()
