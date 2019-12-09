4a43e0574014a41966bdc822dff8e1d5c4edaf23
Piotr Karasiński <piotrkarasinski@078088242237.warszawa.vectranet.pl>
2019-12-09 06:56:03 +0100

    Day 9  solution

--- year2019/day9/a.py
+++ year2019/day9/a.py
@@ -1,7 +1,10 @@
+import copy
 import logging
 import os
 
 from year2019.utils import init_logging
+from year2019.intcode.computer import Computer
+from year2019.intcode.utils import read_program
 
 logger = logging.getLogger(__name__)
 
@@ -12,9 +15,16 @@ def read_input():
 
 
 def main():
-    input = read_input()
-    result = ''
-    logger.info('Result:%s', str(result))
+    program = read_program(os.path.join(
+        os.path.dirname(__file__), "input.txt"))
+    input = [1]
+    computer = Computer(copy.copy(program), input, [])
+    computer.execute()
+    logger.info('Result part a: %s', computer.output)
+    input = [2]
+    computer = Computer(copy.copy(program), input, [])
+    computer.execute()
+    logger.info('Result part b: %s', computer.output)
 
 
 if __name__ == "__main__":
--- year2019/intcode/computer.py
+++ year2019/intcode/computer.py
@@ -6,40 +6,54 @@ logger = logging.getLogger(__name__)
 
 class OpCode:
 
-    def __init__(self, idx, program):
+    def __init__(self, idx, computer):
         self.idx = idx
-        self.op_code = program[idx]
+        self.op_code = computer.program[idx]
+        self.computer = computer
         self.exit = False
 
     @staticmethod
-    def create(program, idx):
-        op_code = program[idx] % 100
+    def create(computer, idx):
+        op_code = computer.program.get(idx, Computer.UNITIALIZED_MEM) % 100
         if op_code == 99:
-            return ExitOp(idx, program)
+            return ExitOp(idx, computer)
         elif op_code == 1:
-            return AddOp(idx, program)
+            return AddOp(idx, computer)
         elif op_code == 2:
-            return MultOp(idx, program)
+            return MultOp(idx, computer)
         elif op_code == 3:
-            return InputOp(idx, program)
+            return InputOp(idx, computer)
         elif op_code == 4:
-            return OutputOp(idx, program)
+            return OutputOp(idx, computer)
         elif op_code == 5:
-            return JumpTrueOp(idx, program)
+            return JumpTrueOp(idx, computer)
         elif op_code == 6:
-            return JumpFalseOp(idx, program)
+            return JumpFalseOp(idx, computer)
         elif op_code == 7:
-            return LessOp(idx, program)
+            return LessOp(idx, computer)
         elif op_code == 8:
-            return EqualsOp(idx, program)
+            return EqualsOp(idx, computer)
+        elif op_code == 9:
+            return RelativeBaseOp(idx, computer)
         else:
             raise ValueError('No such opcode %d' % op_code)
 
-    def get_arg(self, arg_n, program):
+    def read_raw_arg(self, arg_n, positional_ind):
+        program = self.computer.program
+        result = program.get(self.idx + 1 + arg_n, Computer.UNITIALIZED_MEM)
+        if positional_ind == 2:
+            result += self.computer.relative_base
+        return result
+
+    def get_arg(self, arg_n, write=False):
+        program = self.computer.program
         positional_ind = (self.op_code // 10 ** (2 + arg_n)) % 10
-        arg_raw = program[self.idx + 1 + arg_n]
-        if positional_ind == 0:
-            return program[arg_raw]
+        arg_raw = self.read_raw_arg(arg_n, positional_ind)
+        if positional_ind in {0, 2}:
+            if write:
+                return arg_raw
+            else:
+                return program.get(arg_raw, Computer.UNITIALIZED_MEM)
         elif positional_ind == 1:
             return arg_raw
         else:
@@ -51,20 +65,21 @@ class TwoArgResultOpCode(OpCode):
     def exec_func(self, args0, args1):
         raise NotImplementedError()
 
-    def execute_program(self, program, input, output):
-        args0 = self.get_arg(0, program)
-        args1 = self.get_arg(1, program)
-        program[program[self.idx + 3]] = self.exec_func(args0, args1)
+    def execute_program(self, input, output):
+        program = self.computer.program
+        args0 = self.get_arg(0)
+        args1 = self.get_arg(1)
+        program[self.get_arg(2, write=True)] = self.exec_func(args0, args1)
         return self.idx + 4, ComputerState.RUNNING
 
 
 class ExitOp(OpCode):
 
-    def __init__(self, idx, program):
-        super().__init__(idx, program)
+    def __init__(self, idx, computer):
+        super().__init__(idx, computer)
         self.exit = True
 
-    def execute_program(self, program, input, output):
+    def execute_program(self, input, output):
         return self.idx + 1, ComputerState.FINISHED
 
 
@@ -82,17 +97,18 @@ class MultOp(TwoArgResultOpCode):
 
 class InputOp(OpCode):
 
-    def execute_program(self, program, input, output):
+    def execute_program(self, input, output):
+        program = self.computer.program
         if len(input) == 0:
             return self.idx, ComputerState.PAUSED
-        program[program[self.idx + 1]] = input.pop(0)
+        program[self.get_arg(0, write=True)] = input.pop(0)
         return self.idx + 2, ComputerState.RUNNING
 
 
 class OutputOp(OpCode):
 
-    def execute_program(self, program, input, output):
-        args0 = self.get_arg(0, program)
+    def execute_program(self, input, output):
+        args0 = self.get_arg(0)
         output.append(args0)
         return self.idx + 2, ComputerState.RUNNING
 
@@ -102,10 +118,10 @@ class JumpSinlgeArgOpCode(OpCode):
     def compare_arg(self, args0):
         raise NotImplementedError()
 
-    def execute_program(self, program, input, output):
-        args0 = self.get_arg(0, program)
+    def execute_program(self, input, output):
+        args0 = self.get_arg(0)
         if self.compare_arg(args0):
-            return self.get_arg(1, program), ComputerState.RUNNING
+            return self.get_arg(1), ComputerState.RUNNING
         else:
             return self.idx + 3, ComputerState.RUNNING
 
@@ -134,6 +150,14 @@ class EqualsOp(TwoArgResultOpCode):
         return int(args0 == args1)
 
 
+class RelativeBaseOp(OpCode):
+
+    def execute_program(self, input, output):
+        args0 = self.get_arg(0)
+        self.computer.relative_base += args0
+        return self.idx + 2, ComputerState.RUNNING
+
+
 class ComputerState(Enum):
     PAUSED = 1
     RUNNING = 2
@@ -141,12 +165,14 @@ class ComputerState(Enum):
 
 
 class Computer:
+    UNITIALIZED_MEM = 0
 
-    def __init__(self, program, input, output, idx=0):
-        self.program = program
+    def __init__(self, program, input, output, idx=0, relative_base=0):
+        self.program = dict(enumerate(program))
         self.input = input
         self.output = output
         self.idx = idx
+        self.relative_base = relative_base
         self.state = ComputerState.PAUSED
 
     def execute(self):
@@ -154,8 +180,7 @@ class Computer:
             raise ValueError('Can not run computer in state %s' % self.state)
         self.state = ComputerState.RUNNING
         while True:
-            op_code = OpCode.create(self.program, self.idx)
-            self.idx, self.state = op_code.execute_program(
-                self.program, self.input, self.output)
+            op_code = OpCode.create(self, self.idx)
+            self.idx, self.state = op_code.execute_program(self.input, self.output)
             if self.state in [ComputerState.PAUSED, ComputerState.FINISHED]:
                 return
