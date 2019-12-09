659b77f44cf15b9f25c059f1426bc12c037472b9
Piotr Karasinski <piotr.karasinski@beacon.io>
2019-12-09 13:47:59 +0100

    Template for day 10

--- year2019/utils.py
+++ year2019/utils.py
@@ -5,4 +5,4 @@ def init_logging(lvl=logging.INFO):
     logging.basicConfig(
         level=lvl,
         format='[%(asctime)s %(levelname)s %(name)s:%(lineno)d]: %(message)s',
-        datefmt='%m-%d-%Y %H:%M')
+        datefmt='%m-%d-%Y %H:%M:%S')
