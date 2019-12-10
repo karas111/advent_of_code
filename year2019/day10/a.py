af54b74ebb2b6dd05a707d320c1d3458c869d1d9
Piotr Karasinski <piotr.karasinski@beacon.io>
2019-12-10 08:54:14 +0100

    Refactor day 10

--- year2019/day10/a.py
+++ year2019/day10/a.py
@@ -24,67 +24,52 @@ class Asteroid:
 
 
 def read_input():
-    res = set()
-    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
-        for y, line in enumerate(f.readlines()):
-            for x, c in enumerate(line):
-                if c == '#':
-                    res.add(Asteroid(x, y))
-    return res
+    with open(os.path.join(os.path.dirname(__file__), "test2.txt")) as f:
+        return [Asteroid(x, y) for y, line in enumerate(f.readlines()) for x, c in enumerate(line) if c == '#']
 
 
 def calculate_visible(asteroid, others):
-    angles = set()
+    grouped_by_angle = collections.defaultdict(list)
     for o_asteroid in others:
         if o_asteroid == asteroid:
             continue
         angle = asteroid.atan2(o_asteroid)
-        angles.add(angle)
-    return len(angles)
+        grouped_by_angle[angle].append(o_asteroid)
+    return grouped_by_angle
 
 
 def find_best_location(asteroids):
     res = {}
     for asteroid in asteroids:
-        res[asteroid] = calculate_visible(asteroid, asteroids)
-    logging.info('All visibiles %s', res)
+        res[asteroid] = len(calculate_visible(asteroid, asteroids))
     return max(res.items(), key=lambda x: x[1])
 
 
-def count_200(asteroid, asteroids):
-    angles = collections.defaultdict(list)
-    for o_asteroid in asteroids:
-        if o_asteroid == asteroid:
-            continue
-        angle = asteroid.atan2(o_asteroid)
-        angles[angle].append(o_asteroid)
-    for a_l in angles.values():
+def count_nth_asteroid(asteroid, asteroids, counter=200):
+    grouped_by_angle = calculate_visible(asteroid, asteroids)
+    for a_l in grouped_by_angle.values():
         a_l.sort(key=lambda a: (abs(a.x - asteroid.x), abs(a.y - asteroid.y)))
-    angles = list(angles.items())
-    angles.sort(key=lambda x: x[0])
-    # logging.info(angles)
-    idx = next(x for x, group in enumerate(angles) if group[0] >= math.atan2(1, 0))
-    counter = 200
+    grouped_by_angle = list(grouped_by_angle.items())
+    grouped_by_angle.sort(key=lambda x: x[0])
+    idx = next(x for x, group in enumerate(grouped_by_angle) if group[0] >= math.atan2(1, 0))
+    sorted_asteroids = [a_l for angel, a_l in grouped_by_angle]
+
     res = None
     while counter > 0:
-        list_a = angles[idx][1]
+        list_a = sorted_asteroids[idx]
+        idx = (idx+1) % len(sorted_asteroids)
         if not list_a:
-            idx += 1
-            idx %= len(angles)
             continue
-        idx += 1
-        idx %= len(angles)
         res = list_a.pop(0)
         counter -= 1
     return res
-
 
 
 def main():
     asteroids = read_input()
     res = find_best_location(asteroids)
     logger.info('Result part a: %s', res)
-    res = count_200(res[0], asteroids)
+    res = count_nth_asteroid(res[0], asteroids)
     logger.info('Result part b: %s', res)
 
 
