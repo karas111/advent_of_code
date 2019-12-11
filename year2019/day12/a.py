ea2b71ac6e09f48c82bd1fd4398fce0376a4b22e
Piotr Karasiński <piotrkarasinski@078088242237.warszawa.vectranet.pl>
2019-12-11 21:23:45 +0100

    Day 12 template

---
+++ year2019/day12/a.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+from year2019.utils import init_logging
+
+logger = logging.getLogger(__name__)
+
+
+def read_input():
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
+        return [l for l in f]
+
+
+def main():
+    input = read_input()
+    res = None
+    logger.info('Result part a: %s', res)
+    res = None
+    logger.info('Result part b: %s', res)
+
+
+if __name__ == "__main__":
+    init_logging()
+    main()
