333b20388027f405ae421b1ab2f33890df03dfa9
Piotr Karasinski <karas111@gmail.com>
2019-12-12 10:00:37 +0100

    Day 12, part 2

--- year2019/day12/a.py
+++ year2019/day12/a.py
@@ -1,3 +1,4 @@
+import math
 import logging
 import os
 
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 def read_input():
     res = []
     to_replace = ' <>=xyz'
-    with open(os.path.join(os.path.dirname(__file__), "test1.txt")) as f:
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
         for line in f:
             for c in to_replace:
                 line = line.replace(c, '')
@@ -19,27 +20,26 @@ def read_input():
     return numpy.array(res)
 
 
-def simulate(moons, velcocities, max_steps=1000):
+def simulate(moons, velocities, max_steps=1000):
     cache = [{}, {}, {}]
     period = [None]*3
     step = 0
     while max_steps is None or step < max_steps:
-        # logging.info('After %d steps.\nMoons:\n%s\nVelocities:\n%s', step, moons, velcocities)
-        for idx, moon in enumerate(moons):
-            vel_v = numpy.sum(moon < moons, axis=0) - numpy.sum(moon > moons, axis=0)
-            velcocities[idx] += vel_v
-        for idx, moon in enumerate(moons):
-            moons[idx] += velcocities[idx]
         for i in range(3):
             if period[i] is not None:
                 continue
-            t = (tuple(moons[:, i]), tuple(velcocities[:, i]))
-            if t in cache:
+            t = (tuple(moons[:, i]), tuple(velocities[:, i]))
+            if t in cache[i]:
                 period[i] = step - cache[i][t]
             cache[i][t] = step
-        step += 1
-        if all(period):
+        if all(period) and max_steps is None:
             return period
+        for idx, moon in enumerate(moons):
+            vel_v = numpy.sum(moon < moons, axis=0) - numpy.sum(moon > moons, axis=0)
+            velocities[idx] += vel_v
+        for idx, moon in enumerate(moons):
+            moons[idx] += velocities[idx]
+        step += 1
 
 
 def total_energy(moons, velocities):
@@ -48,16 +48,24 @@ def total_energy(moons, velocities):
     return numpy.sum(kin * pot)
 
 
+def lcm(nums):
+    res = 1
+    for n in nums:
+        res = res * n // math.gcd(res, n)
+    return res
+
+
 def main():
     moons = read_input()
     velocities = numpy.zeros(moons.shape, dtype=int)
-    res = simulate(moons, velocities, None)
-    print(res)
-    # logging.info('Moons:\n%s\nVelocities:\n%s', moons, velocities)
-    # res = total_energy(moons, velocities)
-    # logger.info('Result part a: %s', res)
-    # res = None
-    # logger.info('Result part b: %s', res)
+    moons_a, velocities_a = numpy.copy(moons), numpy.copy(velocities)
+    simulate(moons_a, velocities_a, max_steps=1000)
+    logger.info('Moons:\n%s\nVelocities:\n%s', moons_a, velocities_a)
+    res = total_energy(moons_a, velocities_a)
+    logger.info('Result part a: %s', res)
+
+    periods = simulate(numpy.copy(moons), numpy.copy(velocities), max_steps=None)
+    logger.info('Result part b: %d. Periods %s', lcm(periods), str(periods))
 
 
 if __name__ == "__main__":
