4a43d78b214c0afae71bc58bd71128bc851c44c6
Piotr Karasiński <piotrkarasinski@078088242237.warszawa.vectranet.pl>
2019-12-12 07:57:28 +0100

    day 12 - part 2 try

--- year2019/day12/a.py
+++ year2019/day12/a.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def read_input():
     res = []
     to_replace = ' <>=xyz'
-    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
+    with open(os.path.join(os.path.dirname(__file__), "test1.txt")) as f:
         for line in f:
             for c in to_replace:
                 line = line.replace(c, '')
@@ -19,14 +19,27 @@ def read_input():
     return numpy.array(res)
 
 
-def simulate(moons, velcocities, steps=1000):
-    for i in range(steps):
-        logging.info('After %d steps.\nMoons:\n%s\nVelocities:\n%s', i, moons, velcocities)
+def simulate(moons, velcocities, max_steps=1000):
+    cache = [{}, {}, {}]
+    period = [None]*3
+    step = 0
+    while max_steps is None or step < max_steps:
+        # logging.info('After %d steps.\nMoons:\n%s\nVelocities:\n%s', step, moons, velcocities)
         for idx, moon in enumerate(moons):
             vel_v = numpy.sum(moon < moons, axis=0) - numpy.sum(moon > moons, axis=0)
             velcocities[idx] += vel_v
         for idx, moon in enumerate(moons):
             moons[idx] += velcocities[idx]
+        for i in range(3):
+            if period[i] is not None:
+                continue
+            t = (tuple(moons[:, i]), tuple(velcocities[:, i]))
+            if t in cache:
+                period[i] = step - cache[i][t]
+            cache[i][t] = step
+        step += 1
+        if all(period):
+            return period
 
 
 def total_energy(moons, velocities):
@@ -38,12 +51,13 @@ def total_energy(moons, velocities):
 def main():
     moons = read_input()
     velocities = numpy.zeros(moons.shape, dtype=int)
-    simulate(moons, velocities)
-    logging.info('Moons:\n%s\nVelocities:\n%s', moons, velocities)
-    res = total_energy(moons, velocities)
-    logger.info('Result part a: %s', res)
-    res = None
-    logger.info('Result part b: %s', res)
+    res = simulate(moons, velocities, None)
+    print(res)
+    # logging.info('Moons:\n%s\nVelocities:\n%s', moons, velocities)
+    # res = total_energy(moons, velocities)
+    # logger.info('Result part a: %s', res)
+    # res = None
+    # logger.info('Result part b: %s', res)
 
 
 if __name__ == "__main__":
