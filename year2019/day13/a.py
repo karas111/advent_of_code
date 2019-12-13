46ff92ae7fa17f4ae527b034c405ea83eff284a3
Piotr Karasinski <karas111@gmail.com>
2019-12-13 09:00:05 +0100

    Day 13 - part 2

--- year2019/day13/a.py
+++ year2019/day13/a.py
@@ -10,30 +10,53 @@ from year2019.intcode.utils import read_program
 logger = logging.getLogger(__name__)
 
 
-async def create_game(computer):
-    game = {}
-    game_ready = False
-    while not computer.finished or not computer.output.empty():
-        x = await computer.output.get()
-        y = await computer.output.get()
-        item = await computer.output.get()
-        if x == -1 and y == 0:
-            game_ready = True
-            await computer.input.put(0)
-            logger.info('Score P(%d, %d): %d', x, y, item)
-        else:
-            game[(x, y)] = item
-        if game_ready:
-            print_game(game)
-
-    return game
-
-
 EMPTY_I = 0
 WALL = 1
 BLOCK = 2
 PADDLE = 3
 BALL = 4
+
+
+def sign(x):
+    if x < 0:
+        return -1
+    elif x > 0:
+        return 1
+    else:
+        return 0
+
+
+async def create_game(computer):
+    game = {}
+    game_ready = False
+    paddle_x, ball_x = None, None
+    score = 0
+    while not computer.finished or not computer.output.empty():
+        x = await computer.output.get()
+        y = await computer.output.get()
+        item = await computer.output.get()
+        if item in [BALL, PADDLE]:
+            if item == BALL:
+                ball_x = x
+                if game_ready:
+                    await computer.input.put(sign(ball_x-paddle_x))
+                # logger.info('B(%d, %d)', x, y)
+            if item == PADDLE:
+                paddle_x = x
+                # logger.info('P(%d, %d)', x, y)
+
+        if x == -1 and y == 0:
+            if not game_ready:
+                game_ready = True
+                await computer.input.put(sign(ball_x-paddle_x))
+            score = item
+            # logger.info('Score: %d', item)
+        else:
+            game[(x, y)] = item
+        # if game_ready:
+        #     print_game(game)
+
+    return game, score
 
 
 def print_game(game):
@@ -53,14 +76,16 @@ def print_game(game):
 
 
 async def main():
+    logger.info('Start...')
     program = read_program(os.path.join(os.path.dirname(__file__), "input.txt"))
     computer = Computer(copy.copy(program), Queue(), Queue())
-    game, _ = await gather(create_game(computer), computer.execute())
-    logging.info('Result part a: %d', len([v for v in game.values() if v == 2]))
+    (game, _), _ = await gather(create_game(computer), computer.execute())
+    logger.info('Result part a: %d', len([v for v in game.values() if v == 2]))
     program_mod = copy.copy(program)
     program_mod[0] = 2
     computer = Computer(program_mod, Queue(), Queue())
-    game, _ = await gather(create_game(computer), computer.execute())
+    (game, score), _ = await gather(create_game(computer), computer.execute())
+    logger.info('Result part b: %d', score)
 
 
 if __name__ == "__main__":
