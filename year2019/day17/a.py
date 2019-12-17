497ec854fe9897abe453b8b858a3be10f77f36cf
Piotr Karasiński <piotrkarasinski@078088242237.warszawa.vectranet.pl>
2019-12-17 06:54:05 +0100

    Day 17 - parta

--- year2019/day17/a.py
+++ year2019/day17/a.py
@@ -10,15 +10,57 @@ from year2019.utils import init_logging, run_main_coroutine
 logger = logging.getLogger(__name__)
 
 
-async def part1(computer: Computer):
-    return None
+class Node:
+    def __init__(self, x, y, robot_direction):
+        super().__init__()
+        self.neighbours = []
+        self.x = x
+        self.y = y
+        self.robot_direction = robot_direction
+
+    def is_intersection(self):
+        return len(self.neighbours) > 2
+
+    def __repr__(self):
+        return 'N(%d, %d)' % (self.x, self.y)
+
+
+NEIGHB = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+ROBOT_DIECTIONS = {'^': 1, '>': 2, 'v': 3, '<': 4}
+
+
+async def read_graph(computer: Computer):
+    x, y = 0, 0
+    graph = {}
+    while not computer.finished or not computer.output.empty():
+        symbol = await computer.output.get()
+        symbol = chr(symbol)
+        if symbol in ['#', '^', '<', '>', 'v']:
+            node = Node(x, y, robot_direction=ROBOT_DIECTIONS.get(symbol))
+            graph[(x, y)] = node
+            for vect in NEIGHB:
+                neighbour = graph.get((x + vect[0], y + vect[1]))
+                if neighbour is not None:
+                    neighbour.neighbours.append(node)
+                    node.neighbours.append(neighbour)
+
+        elif symbol == '\n':
+            y += 1
+            x = -1
+        x += 1
+    return graph
+
+
+def find_intersections(graph):
+    return sum(n.x * n.y for n in graph.values() if n.is_intersection())
 
 
 async def main():
     logger.info('Start...')
     program = read_program(os.path.join(os.path.dirname(__file__), "input.txt"))
     computer = Computer(copy.copy(program), Queue(), Queue())
-    res, _ = gather(part1(computer), computer.execute())
+    graph, _ = await gather(read_graph(computer), computer.execute())
+    res = find_intersections(graph)
     logger.info('Result part a: %s', res)
     res = None
     logger.info('Result part b: %s', res)
