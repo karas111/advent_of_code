24738fa5ada3dec8ba92f5a835334304e46faa74
Piotr Karasinski <piotr.karasinski@beacon.io>
2019-12-19 08:49:42 +0100

    Day 19 - part2

--- year2019/day19/a.py
+++ year2019/day19/a.py
@@ -1,12 +1,11 @@
 import copy
 import logging
 import os
-from asyncio import Queue, gather, create_task
-from collections import namedtuple
+from asyncio import Queue
 
 from year2019.intcode.computer import Computer
 from year2019.intcode.utils import read_program
-from year2019.utils import init_logging, print_2dgraph, run_main_coroutine
+from year2019.utils import init_logging, print_2darray, run_main_coroutine
 
 logger = logging.getLogger(__name__)
 
@@ -37,33 +36,24 @@ async def check_point(program, x, y):
     return bool(symbol)
 
 
-# async def fits(program, x, y, n):
-#     to_check = [(x-n, y-n), (x+n, y-n)]
-#     for n_x, n_y in to_check:
-#         if not await check_point(program, n_x, n_y):
-#             return False
-#     return True
-
-
 async def find_big(program, n=100):
+    idxoff = n-1
     x = y = 0
-    while not await check_point(program, x-n, y+n):
+    while not await check_point(program, x-idxoff, y+idxoff):
         x += 1
         while not await check_point(program, x, y):
             y += 1
-        if x % 100 == 0:
-            logger.info('TR(%d, %d)', x, y)
-    return (x-n, y)
+    return (x-idxoff, y)
 
 
 async def main():
     logger.info('Start...')
     program = read_program(os.path.join(os.path.dirname(__file__), "input.txt"))
-    # graph = await read_graph(program, 50, 50)
-    # logger.info('Result part a: %s', sum(sum(line) for line in graph))
-    # logger.info('Beam\n%s', '\n'.join(''.join(c and '#' or ' ' for c in line) for line in graph))
-    res = await find_big(program)
-    logger.info('Result part b: %s', res)
+    graph = await read_graph(program, 50, 50)
+    logger.info('Result part a: %s', sum(sum(line) for line in graph))
+    # logger.info('Beam:\n%s', print_2darray(graph, str_func=lambda x: x and '#' or ' '))
+    x, y = await find_big(program, n=100)
+    logger.info('Result part b: (%d, %d) - %d', x, y, x*10000 + y)
 
 
 if __name__ == "__main__":
