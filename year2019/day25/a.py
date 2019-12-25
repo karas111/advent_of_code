4a771e5b8e9f550141f603ceebe7477e2404c1e3
Piotr Karasiński <karas111@gmail.com>
2019-12-25 14:04:40 +0100

    Day 25 - finish the great adventure!

--- year2019/day25/a.py
+++ year2019/day25/a.py
@@ -26,18 +26,48 @@ class GameInterface:
         'photons'
     ]
 
-    def __init__(self, computer: Computer, initial_commands: list):
+    ALL_ITEMS = [
+        'ornament',
+        'loom',
+        'spool of cat6',
+        'wreath',
+        'fixed point',
+        'shell',
+        'candy cane',
+        'weather machine'
+    ]
+
+    def __init__(self, computer: Computer, initial_commands: list, manual=False):
         self.computer = computer
         self.all_commands = []
         self.initial_commands = initial_commands
+        self.manual = manual
 
     async def run(self):
         for com in self.initial_commands:
             await self.send_command(com)
         task = create_task(self.computer.execute())
+        tried_comb = - 1
         while not self.computer.finished:
-            await self.print_computer_output()
-            await self.pass_manual_command()
+            if self.manual:
+                await self.print_computer_output()
+                await self.pass_manual_command()
+            else:
+                tried_comb += 1
+                if tried_comb >= 1 << 9:
+                    raise ValueError('Not found comb')
+                for idx, item in enumerate(self.ALL_ITEMS):
+                    if tried_comb & 1 << idx:
+                        await self.send_command('drop %s' % item)
+                await self.send_command('south')
+                out = await self.print_computer_output()
+                if ('Alert! Droids on this ship are lighter' not in out) and \
+                   ('Alert! Droids on this ship are h' not in out):
+                    return
+                for idx, item in enumerate(self.ALL_ITEMS):
+                    if tried_comb & 1 << idx:
+                        await self.send_command('take %s' % item)
+
         task.cancel()
 
     async def print_computer_output(self):
@@ -48,13 +78,13 @@ class GameInterface:
             buffer_str = ''.join(buffer)
             if buffer_str.endswith('Command?\n') or self.computer.finished:
                 logger.info(buffer_str)
-                return
+                return buffer_str
 
     async def pass_manual_command(self):
         manual_cmd = input('--> ')
         cmd = self.CMD_MAP.get(manual_cmd, manual_cmd)
         if cmd == 'history':
-            logger.info('Commands history:\n%s', ', '.join(self.all_commands))
+            logger.info('Commands history:\n%s', '\'%s\'' % '\', \''.join(self.all_commands))
             await self.pass_manual_command()
         else:
             await self.send_command(cmd)
@@ -72,17 +102,9 @@ async def main():
     program = read_program(os.path.join(os.path.dirname(__file__), "input.txt"))
     computer = Computer(copy.copy(program), Queue(), Queue())
     INITIAL_COMMANDS = [
-        'north',
-        'take wreath',
-        'east',
-        'east',
-        'west',
-        'west',
-        'north',
-        'east'
-
+        'north', 'take wreath', 'east', 'east', 'west', 'west', 'north', 'east', 'south', 'west', 'south', 'south', 'east', 'take loom', 'east', 'take fixed point', 'north', 'take spool of cat6', 'north', 'take weather machine', 'south', 'west', 'take shell', 'east', 'south', 'west', 'south', 'take ornament', 'east', 'south', 'east', 'west', 'south', 'north', 'north', 'west', 'west', 'north', 'take cany cane', 'north', 'inv', 'south', 'take candy cane', 'inv', 'south', 'east', 'north', 'west', 'north', 'north', 'east'
     ]
-    gi = GameInterface(computer, initial_commands=INITIAL_COMMANDS)
+    gi = GameInterface(computer, initial_commands=INITIAL_COMMANDS, manual=False)
     await gi.run()
 
 
