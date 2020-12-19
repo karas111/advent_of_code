59857e1bf4aea8aac546456e536e81a43ef1ac5c
Piotr Karasinski <karas111@gmail.com>
2020-12-19 21:31:14 +0100

    Day19 part2

--- year2020/day19/a.py
+++ year2020/day19/a.py
@@ -1,27 +1,27 @@
-import itertools
 import logging
 import os
 import time
 from typing import Dict, List, Tuple
-from collections import namedtuple
 
 from year2019.utils import init_logging
 
 logger = logging.getLogger(__name__)
 
-INPUT_FILE = "test3.txt"
+INPUT_FILE = "input.txt"
 
 
 class Rule:
     def __init__(self, idx) -> None:
         self.idx = idx
 
-    def match_start(self, line: str, rule_set: Dict[int, 'Rule']):
+    def match_start(self, line: str, rule_set: Dict[int, "Rule"]):
         raise NotImplementedError()
 
     def final_match(self, line: str, rule_set) -> bool:
-        res = next(self.match_start(line, rule_set), None)
-        return res == ""
+        for res in self.match_start(line, rule_set):
+            if res == "":
+                return True
+        return False
 
 
 class LetterRule(Rule):
@@ -29,7 +29,7 @@ class LetterRule(Rule):
         super().__init__(idx)
         self.letter = letter
 
-    def match_start(self, line: str, rule_set: Dict[int, 'Rule']):
+    def match_start(self, line: str, rule_set: Dict[int, "Rule"]):
         if line and line[0] == self.letter:
             yield line[1:]
 
@@ -39,18 +39,19 @@ class MultiRule(Rule):
         super().__init__(idx)
         self.subrules = subrules
 
-    def match_start(self, line: str, rule_set: Dict[int, 'Rule']):
+    def match_start(self, line: str, rule_set: Dict[int, "Rule"]):
         for subrule in self.subrules:
             for res in self.check_subrule(line, subrule, rule_set):
                 yield res
 
-    def check_subrule(self, line: str, subrule: List[int], rule_set: Dict[int, 'Rule']):
-        res = line
-        for rule_id in subrule:
-            res = next(rule_set[rule_id].match_start(res, rule_set), None)
-            if res is None:
-                return
-        yield res
+    def check_subrule(self, line: str, subrule: List[int], rule_set: Dict[int, "Rule"]):
+        if not subrule:
+            yield line
+            return
+        rule = rule_set[subrule[0]]
+        for subline in rule.match_start(line, rule_set):
+            for x in self.check_subrule(subline, subrule[1:], rule_set):
+                yield x
 
 
 def read_input() -> Tuple[List[Rule], List[str]]:
@@ -65,7 +66,7 @@ def read_input() -> Tuple[List[Rule], List[str]]:
 
 def parse_rule(line: str) -> Rule:
     idx, right = line.split(": ")
-    right = right.strip("\"")
+    right = right.strip('"')
     if right.isalpha():
         return LetterRule(int(idx), right)
     subrules = [[int(x) for x in subrule.split(" ")] for subrule in right.split(" | ")]
@@ -76,12 +77,12 @@ def main():
     rules, lines = read_input()
     rule0 = rules[0]
     res = [rule0.final_match(line, rules) for line in lines]
-    logger.info(f"Matching lines {res}, {sum(res)}")
+    logger.info(f"Matching lines, {sum(res)}")
 
-    # rules[8] = MultiRule(8, [[42], [42, 8]])
-    # rules[11] = MultiRule(11, [[42, 31], [42, 11, 31]])
-    # res = [rule0.final_match(line, rules) for line in lines]
-    # logger.info(f"Matching lines {res}, {sum(res)}")
+    rules[8] = MultiRule(8, [[42], [42, 8]])
+    rules[11] = MultiRule(11, [[42, 31], [42, 11, 31]])
+    res = [rule0.final_match(line, rules) for line in lines]
+    logger.info(f"Matching lines, {sum(res)}")
 
 
 if __name__ == "__main__":
