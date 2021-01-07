520c3f1c5966345c8bc4722c2e63de926fe2b6f5
Piotr Karasinski <karas111@gmail.com>
2021-01-07 12:39:27 +0100

    Day 20 2018

--- year2018/day19/day19a.py
+++ year2018/day19/day19a.py
@@ -1,4 +1,9 @@
 from collections import namedtuple
+import os
+import logging
+
+
+INPUT_FILE = "input.txt"
 
 
 class Opcode:
@@ -9,9 +14,9 @@ class Opcode:
         try:
             new_reg = list(registers)
             new_reg[c] = self.calculate_val(new_reg, a, b)
-        except:
+        except Exception:
             print([k for k, v in OPCODES.items() if v == self])
-            print('err')
+            print("err")
             raise
         return new_reg
 
@@ -23,7 +28,7 @@ class ROpcode(Opcode):
     def calculate_val(self, registers, a, b):
         try:
             b_val = registers[b]
-        except:
+        except Exception:
             b_val = None
         return self._action(registers[a], b_val)
 
@@ -60,66 +65,72 @@ eqri = IOpcode(lambda a, b: 1 if a == b else 0)
 eqrr = ROpcode(lambda a, b: 1 if a == b else 0)
 
 OPCODES = {
-    'addr': addr,
-    'addi': addi,
-    'mulr': mulr,
-    'muli': muli,
-    'banr': banr,
-    'bani': bani,
-    'borr': borr,
-    'bori': bori,
-    'setr': setr,
-    'seti': seti,
-    'gtir': gtir,
-    'gtri': gtri,
-    'gtrr': gtrr,
-    'eqir': eqir,
-    'eqri': eqri,
-    'eqrr': eqrr
+    "addr": addr,
+    "addi": addi,
+    "mulr": mulr,
+    "muli": muli,
+    "banr": banr,
+    "bani": bani,
+    "borr": borr,
+    "bori": bori,
+    "setr": setr,
+    "seti": seti,
+    "gtir": gtir,
+    "gtri": gtri,
+    "gtrr": gtrr,
+    "eqir": eqir,
+    "eqri": eqri,
+    "eqrr": eqrr,
 }
 
-Instruction = namedtuple('Instruction', ['opcode', 'a', 'b', 'c'])
+Instruction = namedtuple("Instruction", ["opcode", "a", "b", "c"])
 
-def read_input():
+
+def read_input(filename=None):
     instruction_pointer = None
     program = []
-    with open('input.txt') as f:
+    if filename is None:
+        filename = os.path.join(os.path.dirname(__file__), INPUT_FILE)
+    with open(filename) as f:
         for line in f:
-            if line.startswith('#ip'):
-                instruction_pointer = int(line[len('#ip'):].strip())
+            if line.startswith("#ip"):
+                instruction_pointer = int(line[len("#ip") :].strip())
             else:
-                opcode_str, a, b, c = line.split(' ')
+                opcode_str, a, b, c = line.split(" ")
                 program.append(Instruction(OPCODES[opcode_str], int(a), int(b), int(c)))
 
     return program, instruction_pointer
 
 
+def execute_program(program, ip, registers=None, max_iter=10 ** 15):
+    if registers is None:
+        registers = [1, 0, 0, 0, 0, 0]
 
-def execute_program(program, ip):
-    registers = [1, 0, 0, 0, 0, 0]
-
-    max_iter = 1000
     while registers[ip] < len(program) and max_iter:
         max_iter -= 1
         instruction = program[registers[ip]]
-        registers = instruction.opcode.execute(registers, instruction.a, instruction.b, instruction.c)
+        registers = instruction.opcode.execute(
+            registers, instruction.a, instruction.b, instruction.c
+        )
         registers[ip] += 1
-        print(registers)
+        logging.info(f"Registers {registers}")
     return registers
+
 
 def main():
     program, ip = read_input()
     print(program)
     print(ip)
-    #registers = execute_program(program, ip)
-    #print(registers)
+    # registers = execute_program(program, ip)
+    # print(registers)
 
     n = 10551326
     sum = 0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if n % i == 0:
             sum += i
     print(sum)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
