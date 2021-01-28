2d13fe30b5adb8899e0f3e723477e59faac514d6
Piotr Karasinski <karas111@gmail.com>
2021-01-28 09:49:16 +0100

    Day 16 2016

--- year2016/day15/a.py
+++ year2016/day15/a.py
@@ -1,8 +1,7 @@
-import functools
 import logging
 import os
-import time
 import re
+import time
 
 from year2019.utils import init_logging
 from year2020.day13.a import egcd
@@ -14,9 +13,12 @@ INPUT_FILE = "input.txt"
 
 def parse_input():
     def parse_equation(line):
-        args = re.match(r"Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+).", line).groups()
+        args = re.match(
+            r"Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+).",
+            line,
+        ).groups()
         args = [int(x) for x in args]
-        return (args[1], (-args[0]-args[2]) % args[1])
+        return (args[1], (-args[0] - args[2]) % args[1])
 
     with open(os.path.join(os.path.dirname(__file__), INPUT_FILE)) as f:
         return [parse_equation(line.strip()) for line in f.readlines() if line]
@@ -26,7 +28,9 @@ def tcr(equations):
     current_mod, current_rest = equations[0]
     for mod, rest in equations[1:]:
         _, f, g = egcd(current_mod, mod)
-        current_rest = (current_rest * g * mod + rest * f * current_mod) % (current_mod * mod)
+        current_rest = (current_rest * g * mod + rest * f * current_mod) % (
+            current_mod * mod
+        )
         current_mod *= mod
     return current_mod, current_rest
 
