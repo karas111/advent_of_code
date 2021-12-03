95b29b423339a3d15c077647b88983d6d0a8850f
Piotr Karasinski <karas111@gmail.com>
2021-12-03 16:59:39 +0100

    WIP

--- year2021/day03/a.py
+++ year2021/day03/a.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import List, NamedTuple
+from typing import DefaultDict, List
 
 from year2019.utils import init_logging
 
@@ -14,20 +14,31 @@ def read_numbers():
         return [line.strip() for line in f if line]
 
 
+def find_bit(numbers: List[str], n_bit: int, most_common: bool):
+    count = sum(number[n_bit] == "1" for number in numbers)
+    if (count > len(numbers) - count) ^ most_common:
+        return "1"
+    else:
+        return "0"
+
+
 def find_gamma_epsilon(numbers: List[str]):
-    bit_count = [bit.count("0") for bit in zip(*numbers)]
-    number_len = len(numbers)
-    gamma = ["0" if x * 2 > number_len else "1" for x in bit_count]
-    delta = ["0" if x == "1" else "1" for x in gamma]
-    gamma = int("".join(gamma), base=2)
-    delta = int("".join(delta), base=2)
+    number_len = len(numbers[0])
+    gamma = "".join([find_bit(numbers, n_bit=i, most_common=True) for i in range(number_len)])
+    delta = "".join([find_bit(numbers, n_bit=i, most_common=False) for i in range(number_len)])
     return gamma, delta
+
+
+# def find_co2_oxygen(numbers: List[str], gamma: str, delta: str):
+#     res = copy.copy(numbers)
+#     for bit in range(len(numbers[0])):
 
 
 def main():
     numbers = read_numbers()
     gamma, delta = find_gamma_epsilon(numbers)
-    logger.info(f"Result a {gamma}, {delta}, {gamma * delta}")
+    logger.info(f"Result a {gamma}, {delta}, {int(gamma, base=2) * int(delta, base=2)}")
+    # co2, oxygen = find_co2_oxygen(numbers, gamma, delta)
 
 
 if __name__ == "__main__":
