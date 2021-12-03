08faa239a0ed7b8dfd33cccba4e57ccdb9039ea9
Piotr Karasiński <karas111@gmail.com>
2021-12-03 21:33:19 +0100

    Day 03 b

--- year2021/day03/a.py
+++ year2021/day03/a.py
@@ -1,6 +1,7 @@
 import logging
 import os
-from typing import DefaultDict, List
+import copy
+from typing import List
 
 from year2019.utils import init_logging
 
@@ -16,29 +17,41 @@ def read_numbers():
 
 def find_bit(numbers: List[str], n_bit: int, most_common: bool):
     count = sum(number[n_bit] == "1" for number in numbers)
-    if (count > len(numbers) - count) ^ most_common:
-        return "1"
-    else:
+    if count == len(numbers) - count:
+        return most_common and "1" or "0"
+    elif (count > len(numbers) - count) ^ most_common:
         return "0"
+    else:
+        return "1"
 
 
 def find_gamma_epsilon(numbers: List[str]):
     number_len = len(numbers[0])
-    gamma = "".join([find_bit(numbers, n_bit=i, most_common=True) for i in range(number_len)])
-    delta = "".join([find_bit(numbers, n_bit=i, most_common=False) for i in range(number_len)])
+    gamma = "".join(
+        [find_bit(numbers, n_bit=i, most_common=True) for i in range(number_len)]
+    )
+    delta = "".join(
+        [find_bit(numbers, n_bit=i, most_common=False) for i in range(number_len)]
+    )
     return gamma, delta
 
 
-# def find_co2_oxygen(numbers: List[str], gamma: str, delta: str):
-#     res = copy.copy(numbers)
-#     for bit in range(len(numbers[0])):
+def filter_air(numbers: List[str], most_common: bool):
+    res = copy.copy(numbers)
+    n_bit = 0
+    while len(res) > 1:
+        v_bit = find_bit(res, n_bit=n_bit, most_common=most_common)
+        res = [number for number in res if number[n_bit] == v_bit]
+        n_bit += 1
+    return res[0]
 
 
 def main():
     numbers = read_numbers()
     gamma, delta = find_gamma_epsilon(numbers)
     logger.info(f"Result a {gamma}, {delta}, {int(gamma, base=2) * int(delta, base=2)}")
-    # co2, oxygen = find_co2_oxygen(numbers, gamma, delta)
+    ox, co2 = filter_air(numbers, most_common=True), filter_air(numbers, most_common=False)
+    logger.info(f"Result b {ox}, {co2}, {int(ox, base=2) * int(co2, base=2)}")
 
 
 if __name__ == "__main__":
