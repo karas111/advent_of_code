e9f7bae3801625c89fada6016d4c624a49857741
Piotr Karasinski <piotr.karasinski@beacon.io>
2024-12-11 13:20:34 +0100

    Year 2016 Day 23

--- year2016/day12/a.py
+++ year2016/day12/a.py
@@ -1,7 +1,9 @@
+from abc import abstractmethod
 import functools
 import logging
 import os
 import time
+from typing import Optional
 
 from year2019.utils import init_logging
 
@@ -13,20 +15,34 @@ INPUT_FILE = "input.txt"
 class Instruction:
     def __init__(self, a) -> None:
         self.a = a
+        self._needs_instructions =False
 
+    @abstractmethod
     def execute(self, registers, pointer) -> int:
-        raise NotImplementedError()
+        ...
+
+    def execute_with_insts(self, registers, pointer, insts) -> int:
+        return self.execute(registers, pointer)
 
     def _get_value(self, arg, registers):
         if isinstance(arg, str):
             return registers.get(arg, 0)
         return arg
 
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{type(self).__name__}({self.a})"
+
 
 class TwoArgsInstruction(Instruction):
     def __init__(self, a, b) -> None:
         super().__init__(a)
         self.b = b
+
+    def __str__(self):
+        return f"{type(self).__name__}({self.a}, {self.b})"
 
 
 class IncInst(Instruction):
@@ -55,13 +71,15 @@ class JnzInst(TwoArgsInstruction):
             return pointer + 1
 
 
-def parse_input():
-    INST_MAP = {
+def parse_input(additional_inst_map=None, f=None):
+    inst_map = {
         "cpy": CpyInst,
         "inc": IncInst,
         "dec": DecInst,
         "jnz": JnzInst,
     }
+    if additional_inst_map:
+        inst_map.update(additional_inst_map)
 
     def parse_arg(arg):
         try:
@@ -71,9 +89,11 @@ def parse_input():
 
     def parse_inst(line):
         inst, *args = line.split(" ")
-        return INST_MAP[inst](*[parse_arg(arg) for arg in args])
+        return inst_map[inst](*[parse_arg(arg) for arg in args])
 
-    with open(os.path.join(os.path.dirname(__file__), INPUT_FILE)) as f:
+    if f is None:
+        f = open(os.path.join(os.path.dirname(__file__), INPUT_FILE))
+    with f:
         return [parse_inst(line.strip()) for line in f.readlines() if line]
 
 
