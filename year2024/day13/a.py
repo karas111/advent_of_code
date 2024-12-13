06094d966d9a98b1be90d747dbae934350d3634a
Piotr Karasinski <piotr.karasinski@beacon.io>
2024-12-13 09:49:15 +0100

    Solving Year 2024 Day 13 with z3

--- year2024/day13/a.py
+++ year2024/day13/a.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 from catch_time import catchtime
 from year2019.utils import init_logging
+from z3 import Ints, Solver, sat
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +34,7 @@ def read_input() -> list[Game]:
             yield Game(a, b, prize)
 
 
-def solve(g: Game) -> Optional[int]:
+def solve_m(g: Game) -> Optional[int]:
     b0 = g.prize[0] * g.a[1] - g.prize[1] * g.a[0]
     b1 = g.b[0] * g.a[1] - g.b[1] * g.a[0]
     if b0 % b1 != 0:
@@ -46,13 +47,33 @@ def solve(g: Game) -> Optional[int]:
     return int(a) * 3 + int(b)
 
 
+def solve_z3(g: Game) -> Optional[int]:
+    a, b = Ints("a b")
+    s = Solver()
+    s.add(a >= 0)
+    s.add(b >= 0)
+    s.add(a*g.a[0] + b*g.b[0] == g.prize[0])
+    s.add(a*g.a[1] + b*g.b[1] == g.prize[1])
+    if s.check() != sat:
+        return 0
+    m = s.model()
+    return m[a].as_long() * 3 + m[b].as_long()
+
+
 def main():
     games = list(read_input())
     with catchtime(logger):
-        logger.info("Res A %s", sum(solve(game) for game in games))
+        logger.info("Res A %s", sum(solve_z3(game) for game in games))
         for g in games:
             g.prize = tuple(x + 10000000000000 for x in g.prize)
-        logger.info("Res B %s", sum(solve(game) for game in games))
+        logger.info("Res B %s", sum(solve_z3(game) for game in games))
+
+    games = list(read_input())
+    with catchtime(logger):
+        logger.info("Res A %s", sum(solve_m(game) for game in games))
+        for g in games:
+            g.prize = tuple(x + 10000000000000 for x in g.prize)
+        logger.info("Res B %s", sum(solve_m(game) for game in games))
 
 
 if __name__ == "__main__":
