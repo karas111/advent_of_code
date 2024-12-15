2f3316eacc7561a87d6b683006ec9d526af0ccc2
Piotr Karasinski <piotr.karasinski@beacon.io>
2024-12-15 13:46:00 +0100

    Year 2024 Day 15

---
+++ year2024/day15/a.py
@@ -0,0 +1,155 @@
+import logging
+import os
+
+from catch_time import catchtime
+from year2019.utils import init_logging
+
+logger = logging.getLogger(__name__)
+
+INPUT_FILE = "input.txt"
+
+
+M_VEC = {
+    ">": (1, 0),
+    "<": (-1, 0),
+    "^": (0, -1),
+    "v": (0, 1),
+}
+
+Grid = list[list[str]]
+Cords = tuple[int, int]
+
+
+def read_input() -> tuple[Grid, str]:
+    g = []
+    moves = []
+    with open(os.path.join(os.path.dirname(__file__), INPUT_FILE)) as f:
+        for line in f:
+            if not line.strip():
+                break
+            g.append(list(line.strip()))
+        for line in f:
+            moves.append(line.strip())
+    return g, "".join(moves)
+
+
+def get_current_cord(g: Grid) -> Cords:
+    for y, line in enumerate(g):
+        for x, c in enumerate(line):
+            if c == "@":
+                return x, y
+    raise ValueError("Not found starting pos")
+
+
+def move_a(g: Grid, moves: str) -> Grid:
+    x, y = get_current_cord(g)
+    for m in moves:
+        dx, dy = M_VEC[m]
+        to_move = set()
+        n_x, n_y = x, y
+        move_allowed = True
+        while True:
+            n_x, n_y = n_x + dx, n_y + dy
+            n_c = g[n_y][n_x]
+            if n_c == "O":
+                to_move.add((n_x, n_y))
+            elif n_c == ".":
+                break
+            elif n_c == "#":
+                move_allowed = False
+                break
+        if move_allowed:
+            for n_x, n_y in to_move:
+                g[n_y + dy][n_x + dx] = "O"
+            g[y][x] = "."
+            x, y = x + dx, y + dy
+            g[y][x] = "@"
+        # logger.info("\n%s", "\n".join("".join(line) for line in g))
+
+
+def count_score(g: Grid) -> int:
+    res = 0
+    for y, line in enumerate(g):
+        for x, c in enumerate(line):
+            if c in "O[":
+                res += 100 * y + x
+    return res
+
+
+def modify_map(g: Grid) -> Grid:
+    res = []
+    for line in g:
+        n_line = []
+        res.append(n_line)
+        for c in line:
+            if c in ".#":
+                n_line.extend(c * 2)
+            elif c == "O":
+                n_line.extend("[]")
+            elif c == "@":
+                n_line.extend("@.")
+    return res
+
+
+def move_b(g: Grid, moves: str) -> Grid:
+    x, y = get_current_cord(g)
+    for m in moves:
+        dx, dy = M_VEC[m]
+        to_move = set()
+        n_x, n_y = x, y
+        front_points = {(n_x, n_y)}
+        move_allowed = True
+        while True:
+            new_front_points = set()
+            for n_x, n_y in front_points:
+                n_x, n_y = n_x + dx, n_y + dy
+                n_c = g[n_y][n_x]
+                if n_c in "[]" and dy == 0:
+                    new_front_points.add((n_x, n_y))
+                elif n_c == "[" and y != 0:
+                    new_front_points.add((n_x, n_y))
+                    new_front_points.add((n_x + 1, n_y))
+                elif n_c == "]" and y != 0:
+                    new_front_points.add((n_x, n_y))
+                    new_front_points.add((n_x - 1, n_y))
+                elif n_c == ".":
+                    continue
+                elif n_c == "#":
+                    move_allowed = False
+                    break
+            if not move_allowed:
+                break
+            to_move.update(front_points)
+            front_points = new_front_points
+            if not front_points:
+                break
+        if not move_allowed:
+            continue
+
+        sorted_to_move = sorted(
+            to_move, key=lambda cords: -cords[0] * dx - cords[1] * dy
+        )
+        for n_x, n_y in sorted_to_move:
+            c = g[n_y][n_x]
+            g[n_y][n_x] = "."
+            g[n_y + dy][n_x + dx] = c
+        x, y = x + dx, y + dy
+        # logger.info("\n%s", "\n".join("".join(line) for line in g))
+
+
+def main():
+    g, moves = read_input()
+    with catchtime(logger):
+        move_a(g, moves)
+        logger.info("Res A %s", count_score(g))
+
+    g, moves = read_input()
+    g = modify_map(g)
+    with catchtime(logger):
+        move_b(g, moves)
+        logger.info("Res B %s", count_score(g))
+
+
+if __name__ == "__main__":
+    init_logging()
+    main()
