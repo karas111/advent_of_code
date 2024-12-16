ed1652cf2fe23a3f507504952366e6468eb04239
Piotr Karasinski <piotr.karasinski@beacon.io>
2024-12-16 10:40:34 +0100

    Year 2024 Day 16

--- graph/a_star.py
+++ graph/a_star.py
@@ -1,7 +1,8 @@
 import abc
+from collections import defaultdict
 from numbers import Number
 from queue import PriorityQueue
-from typing import Callable, Iterable, Optional, Protocol, TypeVar
+from typing import Callable, DefaultDict, Iterable, Optional, Protocol, TypeVar, Union
 
 from custom_collections.priority import PriorityEntry
 
@@ -14,30 +15,36 @@ class Graph(Protocol[Node]):
     def neighbours(self, n: Node) -> Iterable[tuple[Node, Number]]: ...
 
 
-def _reconstruct_path(node: Node, came_from: dict[Node, Optional[Node]]) -> list[Node]:
+def _reconstruct_path(node: Node, came_from: dict[Node, set[Node]]) -> tuple[Node, ...]:
     path = []
     while node is not None:
         path.append(node)
-        node = came_from[node]
-    return path[::-1]
+        if came_from[node]:
+            node = came_from[node].pop()
+        else:
+            node = None
+    return tuple(path[::-1])
 
 
 def search_shortest(
-    source: Node, goal: Node, graph: Graph, heurestic: Optional[Callable[[Node, Node], Number]] = None
-) -> tuple[Number, list[Node]]:
+    source: Node, goal: Node, graph: Graph, heurestic: Optional[Callable[[Node, Node], Number]] = None, reconstruct_path: bool = True
+) -> tuple[Number, Union[list[Node]], dict[Node, set[Node]]]:
     if heurestic is None:
         def _heurestic(n1: Node, n2: Node) -> int:
             return 0
         heurestic = _heurestic
     cost_so_far: dict[Node, Number] = {source: 0}
-    came_from: dict[Node, Optional[Node]] = {source: None}
+    came_from: DefaultDict[Node, set[Node]] = defaultdict(set, {source: set()})
     frontier = PriorityQueue()
     frontier.put(PriorityEntry(0, source))
 
     while not frontier.empty():
         current: Node = frontier.get().data
         if current == goal:
-            return cost_so_far[goal], _reconstruct_path(goal, came_from)
+            if reconstruct_path:
+                return cost_so_far[goal], _reconstruct_path(goal, came_from)
+            else:
+                return cost_so_far[goal], came_from
 
         for neighbour, edge_w in graph.neighbours(current):
             new_cost = cost_so_far[current] + edge_w
@@ -45,5 +52,7 @@ def search_shortest(
                 cost_so_far[neighbour] = new_cost
                 priority = new_cost + heurestic(neighbour, goal)
                 frontier.put(PriorityEntry(priority, neighbour))
-                came_from[neighbour] = current
+                # came_from[neighbour] = current
+            if cost_so_far[neighbour] == new_cost:
+                came_from[neighbour].add(current)
     raise ValueError("Not found path")
