2b61ce339a4e4aecdefeb65e9fb3464bbf64fc70
Piotr Karasinski <karas111@gmail.com>
2024-12-20 09:37:57 +0100

    Refactor

--- year2024/day20/a.py
+++ year2024/day20/a.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from collections import defaultdict, deque
-from itertools import product
 
 from catch_time import catchtime
 from cords.cords_2d import Cords
@@ -57,34 +56,18 @@ def solve(g: Grid, max_cheat_size: int) -> int:
     end_depths = bfs(g, end)
     shortest_path = start_depths[end]
     res = defaultdict(int)
-    for cheat_s_x, cheat_s_y in product(range(1, len(g[0]) - 1), range(1, len(g) - 1)):
-        cheat_start = Cords(cheat_s_x, cheat_s_y)
-        if g[cheat_start.y][cheat_start.x] == "#":
-            continue
-        for cheat_size in range(2, max_cheat_size + 1):
-            for add_x in range(cheat_size + 1):
-                seen_cheat_end = set()
-                for mult_x, mult_y in [(1, -1), (1, 1), (-1, -1), (-1, 1)]:
-                    cheat_end = Cords(
-                        cheat_s_x + add_x * mult_x,
-                        cheat_s_y + (cheat_size - add_x) * mult_y,
-                    )
-                    if cheat_end in seen_cheat_end:
-                        continue
-                    seen_cheat_end.add(cheat_end)
-                    if (
-                        cheat_end.x < 0
-                        or cheat_end.x >= len(g[0])
-                        or cheat_end.y < 0
-                        or cheat_end.y >= len(g)
-                        or g[cheat_end.y][cheat_end.x] == "#"
-                    ):
-                        continue
-                    new_shortest = (
-                        cheat_size + start_depths[cheat_start] + end_depths[cheat_end]
-                    )
-                    if new_shortest < shortest_path:
-                        res[shortest_path - new_shortest] += 1
+    for cheat_start, depth_from_start in start_depths.items():
+        for add_x in range(-max_cheat_size, max_cheat_size + 1):
+            max_add_y = max_cheat_size - abs(add_x)
+            for add_y in range(-max_add_y, max_add_y + 1):
+                cheat_end = Cords(cheat_start.x + add_x, cheat_start.y + add_y)
+                if cheat_end not in start_depths:
+                    continue
+                new_shortest = (
+                    abs(add_x) + abs(add_y) + depth_from_start + end_depths[cheat_end]
+                )
+                if new_shortest < shortest_path:
+                    res[shortest_path - new_shortest] += 1
     return sum(v for k, v in res.items() if k >= 100)
 
 
