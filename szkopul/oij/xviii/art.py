970c3bd448c568738c4b6a28d3981bf2947a5cc5
Piotr Karasinski <karas111@gmail.com>
2025-11-10 10:03:03 +0100

    Szkopul puzzles

--- szkopul/oij/xviii/art.py
+++ szkopul/oij/xviii/art.py
@@ -1,10 +1,9 @@
 def main():
-    art_n = int(input())
+    art_n = int(input()) - 1
     z_n = art_n // 26
-    res = "z" * z_n
-    if art_n % 26 != 0:
-        res = res + chr(ord("a") + (art_n % 26) - 1)
-    print(res)
+    res_z = "z" * z_n
+    res_last = chr(ord("a") + (art_n % 26))
+    print(res_z + res_last)
 
 
 if __name__ == "__main__":
