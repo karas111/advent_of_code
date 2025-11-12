70b3533de2eb072582141bad9b71b6c0bfe119d6
Piotr Karasinski <piotr.karasinski@beacon.io>
2025-11-12 11:51:52 +0100

    Add comments for oij solutions

--- szkopul/oij/xviii/art.py
+++ szkopul/oij/xviii/art.py
@@ -1,8 +1,8 @@
 def main():
-    art_n = int(input()) - 1
-    z_n = art_n // 26
+    art_n = int(input()) - 1  # numerujemy od 0
+    z_n = art_n // 26  # ile razy "z" na poczatku
     res_z = "z" * z_n
-    res_last = chr(ord("a") + (art_n % 26))
+    res_last = chr(ord("a") + (art_n % 26))  # ostatnia litera, moze tez być "z"
     print(res_z + res_last)
 
 
