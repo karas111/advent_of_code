9bb5e66e214b538e96f53edfce8b7ccfa780211a
Piotr Karasinski <karas111@gmail.com>
2025-12-12 16:16:29 +0100

    Year 2022 Day 22 - hardest problem in AOC ever

--- year2022/day22/a.py
+++ year2022/day22/a.py
@@ -1,13 +1,10 @@
-import bisect
 import logging
-import operator
 import os
 import re
-from collections import namedtuple
+from dataclasses import dataclass
 from typing import NamedTuple
 
-from sympy import Eq, symbols
-from sympy.solvers import solve
+import numpy as np
 
 from catch_time import catchtime
 from cords.cords_2d import Cords
@@ -29,7 +26,7 @@ def read_input() -> tuple[list[str], str]:
     grid = []
     with open(os.path.join(os.path.dirname(__file__), INPUT_FILE)) as f:
         for line in f:
-            line = line.rstrip()
+            line = line.rstrip("\n")
             if not line:
                 break
             grid.append(line)
@@ -40,7 +37,7 @@ def read_input() -> tuple[list[str], str]:
         return grid, moves
 
 
-def solve(grid: list[str], moves: list):
+def solve_a(grid: list[str], moves: list):
     state = State(Cords(grid[0].index("."), 0), 0)
 
     max_l = max(len(line) for line in grid)
@@ -99,13 +96,155 @@ def solve(grid: list[str], moves: list):
     return state
 
 
+@dataclass
+class Cube:
+    size: int
+    faces: dict
+
+    #   e----f
+    #  /|   /|
+    # a-+--b |
+    # | g--+-h
+    # |/   |/
+    # c----d
+
+    # We will store the original coordinates on each face of the Cube as 2d matrix.
+    # Cube faces are named "abcd", "bfdh", "fehg", "eagc", "efab", "ghcd"
+
+    FACES = ["abcd", "bfdh", "fehg", "eagc", "efab", "ghcd"]
+
+    def rotate(self, dir: str):
+        new_faces = {}
+        if dir in "><":
+            replaces = {"abcd": "bfdh", "bfdh": "fehg", "fehg": "eagc", "eagc": "abcd"}
+            if dir == "<":
+                new_faces = {a: self.faces[b] for a, b in replaces.items()}
+            else:
+                new_faces = {b: self.faces[a] for a, b in replaces.items()}
+
+            # rotate counter clock wise
+            k = 1 if dir == ">" else -1
+            new_faces["efab"] = np.rot90(self.faces["efab"], k=k)
+            new_faces["ghcd"] = np.rot90(self.faces["ghcd"], k=k)
+        elif dir in "^v":
+            if dir == "^":
+                new_faces["abcd"] = np.flipud(self.faces["ghcd"])
+                new_faces["ghcd"] = np.fliplr(self.faces["fehg"])
+                new_faces["fehg"] = np.flipud(np.fliplr(self.faces["efab"]))
+                new_faces["efab"] = self.faces["abcd"]
+            else:
+                new_faces["ghcd"] = np.flipud(self.faces["abcd"])
+                new_faces["fehg"] = np.fliplr(self.faces["ghcd"])
+                new_faces["efab"] = np.flipud(np.fliplr(self.faces["fehg"]))
+                new_faces["abcd"] = self.faces["efab"]
+            k = 1 if dir == "v" else -1
+            new_faces["bfdh"] = np.rot90(self.faces["bfdh"], k=k)
+            new_faces["eagc"] = np.rot90(self.faces["eagc"], k=-k)
+        return Cube(self.size, new_faces)
+
+
+MOVES_B = {">": Cords(1, 0), "v": Cords(0, 1), "<": Cords(-1, 0), "^": Cords(0, -1)}
+
+
+def solve_b(grid: list[str], moves: list):
+    size = 50 if INPUT_FILE == "input.txt" else 4
+    start = Cords(grid[0].index("."), 0)
+    cube = Cube(
+        size,
+        faces={
+            face_name: np.full((size, size), None, dtype=object)
+            for face_name in Cube.FACES
+        },
+    )
+    seen = set()
+    roll_cube = {"^": "^", "v": "v", "<": ">v<", ">": "<v>"}
+    deroll_cube = {"^": "v", "v": "^", "<": ">^<", ">": "<^>"}
+
+    def dfs(cords: Cords, cube: Cube):
+        if cords in seen:
+            return cube
+        seen.add(cords)
+        cube.faces["ghcd"] = np.array(
+            [
+                [Cords(cords.x + dx, cords.y + dy) for dx in range(size)]
+                # [grid[cords.y + dy][cords.x + dx] for dx in range(size)]
+                for dy in range(size)
+            ]
+        )
+        for move, dc in MOVES_B.items():
+            neighbour = cords + dc * size
+            if (
+                neighbour in seen
+                or neighbour.y < 0
+                or neighbour.y >= len(grid)
+                or neighbour.x < 0
+                or neighbour.x >= len(grid[neighbour.y])
+                or grid[neighbour.y][neighbour.x] == " "
+            ):
+                continue
+
+            rolls = roll_cube[move]
+            derolls = deroll_cube[move]
+            for rot in rolls:
+                cube = cube.rotate(rot)
+            cube = dfs(neighbour, cube)
+            for rot in derolls:
+                cube = cube.rotate(rot)
+        return cube
+
+    cube = dfs(start, cube)
+    # we started with the first cube's face at the bottom. Rotate
+    # it, so the starting point is in bottom left corner of front ("abcd") face
+    cube = cube.rotate(">").rotate("^")
+    cords = Cords(0, 0)
+    direction = 1  # "v"
+    dir_mod = -1
+    for move in moves:
+        if move == "L":
+            direction = (direction - 1 * dir_mod) % 4
+        elif move == "R":
+            direction = (direction + 1 * dir_mod) % 4
+        else:
+            dc = MOVES[direction]
+            for _ in range(move):
+                n_cube_cords = cords + dc
+                n_cube = cube
+                # if we cross an edge, we rotate the cube, so the "pointer" is always on front ("abcd") face
+                if n_cube_cords.x < 0:
+                    n_cube = cube.rotate(">")
+                elif n_cube_cords.x >= size:
+                    n_cube = cube.rotate("<")
+                elif n_cube_cords.y < 0:
+                    n_cube = cube.rotate("v")
+                elif n_cube_cords.y >= size:
+                    n_cube = cube.rotate("^")
+                n_cube_cords = Cords(n_cube_cords.x % size, n_cube_cords.y % size)
+                net_cords = n_cube.faces["abcd"][n_cube_cords.y][n_cube_cords.x]
+
+                is_block = grid[net_cords.y][net_cords.x] == "#"
+                if is_block:
+                    break
+                cords = n_cube_cords
+                cube = n_cube
+    net_cords = cube.faces["abcd"][cords.y][cords.x]
+    prev_cords = cords - MOVES[direction]
+    prev_net_cords = cube.faces["abcd"][prev_cords.y][prev_cords.x]
+    d_net_vect = net_cords - prev_net_cords
+    d_net_dir = MOVES.index(d_net_vect)
+    logger.info("Last cords %s, dir %s", net_cords, d_net_dir)
+    logger.info(
+        "Res b: %d", (net_cords.y + 1) * 1000 + (net_cords.x + 1) * 4 + d_net_dir
+    )
+
+
 def main():
     grid, moves = read_input()
-    state = solve(grid, moves)
+    state = solve_a(grid, moves)
     logger.info(
         "Result a %d",
         (state.cords.y + 1) * 1000 + (state.cords.x + 1) * 4 + state.direction,
     )
+    solve_b(grid, moves)
 
 
 if __name__ == "__main__":
